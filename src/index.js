145b39156bc04dda1584645aff7bf9fa5ea634dc
Till Willing <tillwilling@gmail.com>
2021-04-27 12:32:40 +0200

    created js-intro

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import AppHeader from './components/AppHeader'
+console.clear()
+const box = document.querySelector('.box')
+const buttonAdd = document.querySelector('.button-add')
+const buttonRemove = document.querySelector('.button-remove')
+const buttonToggle = document.querySelector('.button-toggle')
 
-document.body.append(AppHeader('JavaScript App Template'))
+buttonAdd.addEventListener('click', () => {
+  box.classList.add('blue')
+})
+
+buttonRemove.addEventListener('click', () => {
+  box.classList.remove('blue')
+})
+
+buttonToggle.addEventListener('click', () => {
+  box.classList.toggle('blue')
+})
