f3f64175a4a79bb4d4b39a834c43be60860e14df
Till Willing <tillwilling@gmail.com>
2021-04-27 13:12:39 +0200

    rotate, background, radius buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 console.clear()
 const box = document.querySelector('.box')
-const buttonAdd = document.querySelector('.button-add')
-const buttonRemove = document.querySelector('.button-remove')
-const buttonToggle = document.querySelector('.button-toggle')
+const buttonRotate = document.querySelector('.button-rotate')
+const buttonBackground = document.querySelector('.button-background')
+const buttonRadius = document.querySelector('.button-radius')
 
-buttonAdd.addEventListener('click', () => {
-  box.classList.add('blue')
+buttonRotate.addEventListener('click', () => {
+  box.classList.toggle('rotate')
 })
 
-buttonRemove.addEventListener('click', () => {
-  box.classList.remove('blue')
-})
-
-buttonToggle.addEventListener('click', () => {
+buttonBackground.addEventListener('click', () => {
   box.classList.toggle('blue')
+})
+
+buttonRadius.addEventListener('click', () => {
+  box.classList.toggle('radius')
 })
